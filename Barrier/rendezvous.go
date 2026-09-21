ee69e41bee23f93ab65799bc5749c37331f2df4b
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-21 12:59:46 +0100

    template rendezvous

--- Barrier/rendezvous.go
+++ Barrier/rendezvous.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand/v2"
+	"sync"
+	"time"
+)
+
+func WorkWithRendezvous(wg *sync.WaitGroup, Num int) bool {
+	var X time.Duration
+	X = time.Duration(rand.IntN(5))
+	time.Sleep((X * time.Second))
+	fmt.Println("Part A", Num)
+	// rendezvous here
+	fmt.Println("Part B", Num)
+	wg.Done()
+	return true
+}
+
+func main() {
+	var wg sync.WaitGroup
+	threadCount := 5
+
+	wg.Add(threadCount)
+	for N := range threadCount {
+		go WorkWithRendezvous(&wg, N)
+	}
+	wg.Wait()
+}
