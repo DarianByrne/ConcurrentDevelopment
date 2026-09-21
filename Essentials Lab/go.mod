40069749e5d832ea9d9e80d46fc65f455946c134
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-21 12:46:22 +0100

    sem-ex.go works!

---
+++ Essentials Lab/go.mod
@@ -0,0 +1,5 @@
+module Essentials_Lab
+
+go 1.27
+
+require golang.org/x/sync v0.23.0
--- Essentials Lab/sem-ex.go
+++ Essentials Lab/sem-ex.go
@@ -1,3 +1,67 @@
+// Author: Darian Byrne
+// License: GPL3
+// Help received from: Milosz Cwynar, Oliwier Jakubiec, Mykhailo Balaker
+
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"log"
+	"runtime"
+
+	"golang.org/x/sync/semaphore"
+)
+
+func main() {
+	ctx := context.TODO()
+
+	var (
+		maxWorkers = runtime.GOMAXPROCS(0)
+		sem        = semaphore.NewWeighted(int64(maxWorkers))
+		out        = make([]int, 64)
+	)
+
+	for i := range out {
+		if err := sem.Acquire(ctx, 1); err != nil {
+			log.Printf("Failed to aquire semaphore: %v", err)
+			break
+		}
+
+		go func(i int) {
+			defer sem.Release(1)
+			out[i] = collatzSteps(i + 1)
+		}(i)
+	}
+
+	if err := sem.Acquire(ctx, int64(maxWorkers)); err != nil {
+		log.Printf("Failed to acquire semaphore: %v", err)
+	}
+
+	fmt.Println(out)
+}
+
+func collatzSteps(n int) (steps int) {
+	if n <= 0 {
+		panic("nonpositive input")
+	}
+
+	for ; n > 1; steps++ {
+		if steps < 0 {
+			panic("too many steps")
+		}
+
+		if n%2 == 0 {
+			n /= 2
+			continue
+		}
+
+		const maxInt = int(^uint(0) >> 1)
+		if n > (maxInt-1)/3 {
+			panic("overflow")
+		}
+		n = 3*n + 1
+	}
+
+	return steps
+}
