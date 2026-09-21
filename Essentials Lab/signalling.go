44da5f763b178bbb06fa3d59dd4ff62ba2921707
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-21 12:03:10 +0100

    signaling also works

--- Essentials Lab/signalling.go
+++ Essentials Lab/signalling.go
@@ -1,5 +1,42 @@
+// Author: Darian Byrne
+// License: GPL3
+// Help received from: Milosz Cwynar
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	barrier := make(chan bool)
+
+	doStuffOne := func() bool {
+		fmt.Println("StuffOne - Part A")
+
+		barrier <- true
+
+		fmt.Println("StuffOne - Part B")
+		wg.Done()
+		return true
+	}
+
+	doStuffTwo := func() bool {
+		time.Sleep(time.Second * 5)
+		fmt.Println("StuffTwo - Part A")
+
+		<-barrier
+
+		fmt.Println("StuffTwo - Part B")
+		wg.Done()
+		return true
+	}
+
+	wg.Add(2)
+	go doStuffOne()
+	go doStuffTwo()
+	wg.Wait()
 }
