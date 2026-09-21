f25b24f1c4b2ed8ea9c0249ef9f41370ae2d64bf
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-21 11:25:49 +0100

    mutex works

--- Essentials Lab/mutex.go
+++ Essentials Lab/mutex.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+var total int64
+
+func adds(n int, theLock *sync.Mutex) bool {
+	for i := 0; i < n; i++ {
+		theLock.Lock()
+		total++
+		theLock.Unlock()
+	}
+	wg.Done()
+	return true
+}
+
+func main() {
+	var theLock sync.Mutex
+
+	total = 0
+	wg.Add(10)
+
+	for i := range 10 {
+		fmt.Println(i)
+		go adds(1000, &theLock)
+	}
+	wg.Wait()
+	fmt.Println(total) // 10000
+}
