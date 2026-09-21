080f4d77fe6945550f50a837994ab1e26a78b693
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-21 12:15:19 +0100

    semephore.go works!

--- Essentials Lab/semaphore.go
+++ Essentials Lab/semaphore.go
@@ -1,3 +1,36 @@
+// Author: Darian Byrne
+// License: GPL3
+// Help received from: Seamus Kennedy
+
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// this type is unused? so i commented it out to make the IDE happy
+//type semaphore struct {
+//	theCounter chan struct{}
+//}
+
+// funcAcquite(sem *Semaphore) // what is this?
+func main() {
+	maxGoRoutines := 5
+	semaphore := make(chan struct{}, maxGoRoutines)
+
+	var wg sync.WaitGroup
+	for i := range 20 {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			semaphore <- struct{}{}
+			defer func() { <-semaphore }()
+
+			fmt.Printf("Running task %d\n", i)
+			time.Sleep(2 * time.Second)
+		}(i)
+	}
+	wg.Wait()
+}
