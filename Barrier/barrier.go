6b97708aa2db071059b4d28c5510a86ef6f3d413
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2026-09-23 11:45:31 +0100

    template barrier

--- Barrier/barrier.go
+++ Barrier/barrier.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+func doStuff(goNum int, wg *sync.WaitGroup) bool {
+	time.Sleep(time.Second)
+	fmt.Println("Part A", goNum)
+	// barrier here
+	fmt.Println("Part B", goNum)
+	wg.Done()
+	return true
+}
+
+func main() {
+	totalRoutines := 10
+	var wg sync.WaitGroup
+	wg.Add(totalRoutines)
+	ctx := context.TODO()
+	var theLock sync.Mutex
+	sem := semaphore.NewWeighted(int64(totalRoutines))
+	theLock.Lock()
+	sem.Acquire(ctx, 1)
+	for i := range totalRoutines {
+		go doStuff(i, &wg)
+	}
+	sem.Release(1)
+	theLock.Unlock()
+
+	wg.Wait()
+}
---
+++ Barrier/go.mod
@@ -0,0 +1,5 @@
+module Barrier
+
+go 1.27
+
+require golang.org/x/sync v0.23.0
